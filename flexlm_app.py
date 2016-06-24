5f0b79cfb5f9b1da78be23fa8c6f033dee41dc79
Andrew Rae <andrew3092@gmail.com>
2016-06-24 14:08:26 +1200

    Added Roles and Users table to the database

--- flexlm_app.py
+++ flexlm_app.py
@@ -12,6 +12,8 @@ from wtforms import StringField, FileField, SubmitField, IntegerField
 from wtforms import ValidationError, widgets, SelectMultipleField, BooleanField
 from wtforms.validators import Required
 from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
 import json
 import flexlm_parser
 import rrdfetch
@@ -52,6 +54,39 @@ class Columns(db.Model):
     def __repr__(self):
         return '<Columns %r>' % self.columns
 
+# This database model has been taken from the book flask web development by
+# Miguel Grinberg
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
 class baseForm(Form):
     vendor = StringField('Vendor', validators=[Required()])
     port = IntegerField('Port Number', validators=[Required()])
