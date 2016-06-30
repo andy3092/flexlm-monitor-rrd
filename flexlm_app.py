4972b78d843246f1bd585cddfe408906195e8854
Andrew Rae <andrew3092@gmail.com>
2016-07-01 10:53:19 +1200

    Stripped out the roles table from the database model

--- flexlm_app.py
+++ flexlm_app.py
@@ -64,29 +64,16 @@ class Columns(db.Model):
     def __repr__(self):
         return '<Columns %r>' % self.columns
 
-# This database model has been taken from the book flask web development by
-# Miguel Grinberg
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-
 #-------------------------------------------------------------------
 # Authentication implentation and SQLAlchemy model
+# This database model has been taken from the book flask web development by
+# Miguel Grinberg
 #-------------------------------------------------------------------
-# The following dictionary acts as a dummy database for the moment
-# get it up and running first then implment the proper hashing
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
 
     @property
